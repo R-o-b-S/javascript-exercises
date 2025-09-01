09d3735db60c6bec1844c744bf64330b23a570aa
R-o-b-S <189221924+R-o-b-S@users.noreply.github.com>
2025-09-01 22:11:34 +0200

    06:repeat_String completed

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function repstring (string, num) {
+    let text = "";
+    if (num < 0)
+    {return "ERROR";}
+    else {
+        for (let i = 0; i < num; i++) {
+        text = text + string ;
+        }
+        return text ;
+    }
 };
 
 // Do not edit below this line
