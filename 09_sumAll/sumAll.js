960b50bb725a9b84a136250260a82a0a9ff3168e
R-o-b-S <189221924+R-o-b-S@users.noreply.github.com>
2026-05-07 07:45:28 +0200

    Ex.09 completed!

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let sum = 0;
+    if (x>0 && y>0 && Number.isInteger(x) && Number.isInteger(y)) 
+    {
+        if (x<=y){
+            for (i=x; i<=y; i++){
+            sum=sum+i;
+            }
+        }
+        else if (x>y){
+            for (i=y; i<=x; i++){
+            sum=sum+i;
+            }
+        }
+        return sum;
+    }
+    else {return "ERROR";}
+    
 };
 
 // Do not edit below this line
