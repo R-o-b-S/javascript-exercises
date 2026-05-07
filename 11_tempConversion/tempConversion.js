cfd6c7da3ea69f9866dab0219429308cf901bcd1
R-o-b-S <189221924+R-o-b-S@users.noreply.github.com>
2026-05-07 08:21:04 +0200

    ex. 11 completed!

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  let cel = Math.round(((far-32)*5/9)*10)/10;
+  return cel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let far = Math.round(((cel*9/5)+32)*10)/10;
+  return far;
 };
 
 // Do not edit below this line
