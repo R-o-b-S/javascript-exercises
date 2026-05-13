3fe25f92bf8c8e2eb525e84c8a15d33cbbe2c371
R-o-b-S <189221924+R-o-b-S@users.noreply.github.com>
2026-05-13 17:32:04 +0200

    completed

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (w) {
+    function reverse (a) {
+        return a.split("").reverse().join("");
+    }
+    function punctuation (e) {
+        return e.replace(/[!,.\s]/g, "");
+    }
+    const wor = punctuation(w).toLowerCase();
+    const rev = reverse(wor);
+    if (rev === wor) {return true;}
+    else {return false;}
 };
 
 // Do not edit below this line
