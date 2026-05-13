9e711135a8b67f225e85096a438724ff3f8b882d
R-o-b-S <189221924+R-o-b-S@users.noreply.github.com>
2026-05-13 19:20:33 +0200

    done!

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(b) {
+    const books = b;
+    const titles = books.map(
+        function(title)
+        {return title["title"];})
+    return titles;
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
