302c95f9d6f3590901f4f71684cd596264e0c8e4
R-o-b-S <189221924+R-o-b-S@users.noreply.github.com>
2026-05-13 18:54:37 +0200

    success

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(i) {
+    if (i < 0) {return "OOPS";}
+    else {
+    let fib = [0, 1, 1];
+    for (c = 1; c < 26 ; c++)
+    {
+        let a = fib[c]+fib[c+1];
+        fib.push(a);
+    }
+    return fib[i];
+    }
+    
 };
 
 // Do not edit below this line
