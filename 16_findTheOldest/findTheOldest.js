1b108c0987b8ef2771f5a78a70fe2d5f05757261
R-o-b-S <189221924+R-o-b-S@users.noreply.github.com>
2026-05-15 17:44:47 +0200

    completed

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(p) {
+    const people = p;
+    for (i=0; i<people.length; i++){
+        if (people[i].yearOfDeath === undefined) {
+            const yn = new Date();
+            let yd = yn.getFullYear();
+            people[i].yearOfDeath = yd;}
+    }
+    let old = people["0"];
+    for (i=1; i<people.length; i++){
+        let a = old.yearOfDeath - old.yearOfBirth;
+        let b = people[i].yearOfDeath - people[i].yearOfBirth;
+        if (b > a) {old = people[i];}
+    }
+    return old;
 
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
+
+
